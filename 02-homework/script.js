228f5665f7659dfc1b54edbe44d2bce820c3e92c
Vadim <vadim270389@gmail.com>
2022-02-20 16:25:29 +0200

    homework-02 fix error when enter NaN and not an integer

--- 02-homework/script.js
+++ 02-homework/script.js
@@ -1,24 +1,24 @@
 let startNumber
 do {
     startNumber = +prompt("Введите начальное число: N", 0)
-} while (!Number.isInteger(startNumber))
+    if (Number.isNaN(startNumber) || !Number.isInteger(startNumber)) alert('Вы ввели не числовое значение!!! Введите пожалуста целое число')
+} while (Number.isNaN(startNumber) || !Number.isInteger(startNumber))
 
 let endNumber
 do {
     endNumber = +prompt("Введите конечное число: М", 0)
-} while (!Number.isInteger(endNumber))
+    if (Number.isNaN(endNumber) || !Number.isInteger(endNumber)) alert('Вы ввели не числовое значение!!! Введите пожалуста целое число')
+} while (Number.isNaN(endNumber) || !Number.isInteger(endNumber))
 
 let checkBoolean = confirm("Хотите ли Вы пропускать парные числа ?")
 let sum = 0;
 
 function sumInterval() {
-    let N = Math.min(startNumber,endNumber)
-    let M = Math.max(startNumber,endNumber)
+    let N = Math.min(startNumber, endNumber)
+    let M = Math.max(startNumber, endNumber)
     for (let i = N; i <= M; i++) {
-        if (checkBoolean === true) {
-            if (i % 2) {
-                sum += i
-            }
+        if (checkBoolean === true && i % 2) {
+            sum += i
         } else {
             sum += i
         }
